2e24d6e5c14781e435fc157379e7e48a4a45a052
root <root@ip-172-31-39-218.ap-northeast-1.compute.internal>
2023-02-05 20:57:28 +0000

    docker_usecase for commit images

--- script.sh
+++ script.sh
@@ -5,47 +5,47 @@ i=1
 while [[ $i -lt 3 ]]
 do
 	clear
-	echo "-------------------------------------------------------------"
-	echo "************ $i.Process Of Container Started ***************"
-	echo "--------------------------------------------------------------"
-	echo "--------------------------------------------------------------"
-        read -p " Enter the name you want to install inside container:->" soft
-        echo "---------------------------------------------------------------"
-        echo "FROM ubuntu" >> Dockerfile
-        echo " RUN apt-get -y update && apt-get -y install $soft " >> Dockerfile
-	echo "------------------------------------------------------"
-        read -p "Enter the name of image you want to build:->" img
-	echo "------------------------------------------------------"
-        docker build -t $img .
-	echo "------------------------------------------------------"
-	read -p "Enter the name of container you want to run:->" con
-	echo "------------------------------------------------------"
-        docker container run -dt --name $con $img
-	echo "----------------------------------------------------------------"
-	echo "****************Container Start Running*******************"
-	echo "----------------------------------------------------------------"
-        read -p "Enter the name of image you to want to save container:->" img1
-	echo "----------------------------------------------------------------"
+	echo "-----------------------------------------------------------------------------"
+	echo "******************** $i.Process Of Container Started ************************"
+	echo "-----------------------------------------------------------------------------"
+        read -p "Enter the name of container you want to run:->" con
+	echo "-----------------------------------------------------------------------------"
+        docker container run -dt --name $con ubuntu
+	echo "-----------------------------------------------------------------------------"
+	echo "*************************Container Start Running*****************************"
+	echo "-----------------------------------------------------------------------------"
+	docker exec --user root $con apt-get update -y
+	read -p "Enter the name you want to install inside container:->" install
+	docker exec --user root $con apt-get install $install -y
+	echo "-----------------------------------------------------------------------------"
+	echo "-----------------$install in install inside container------------------------"
+	echo "-----------------------------------------------------------------------------"
+        read -p "Enter the name of image you want to use to save container:->" img1
+	echo "-----------------------------------------------------------------------------"
         docker container commit $con $img1
-        truncate -s 0 Dockerfile
-        echo "FROM $img1" >> Dockerfile
-        echo "RUN dd if=/dev/zero of=/root/file1.txt bs=1M count=500" >> Dockerfile
-	echo "--------------------------------------------------------------------"
-        read -p "Enter the image name to want to build using previous one:->" img2 
-	echo "--------------------------------------------------------------------"
-	docker build -t $img2 .
-	echo "---------------------------------------------------------------"
-	read -p " Enter the name of container you want to run again:-> " con1
-	echo "---------------------------------------------------------------"
-	docker container run -dt --name $con1 $img2
-	echo "----------------------------------------------------------------------"
-	read -p "Enter the name of image you want to save with final chnages:->" img3
-	echo "----------------------------------------------------------------------"
-	docker container commit $con1 $img3 
-	docker image history $img3 >> $img3.log.txt
-	echo "----------------------------------"
-	echo "Image History sent to $img.log.txt"
-	echo "----------------------------------"
-	truncate -s 0 Dockerfile
+	echo "-----------------------------------------------------------------------------"
+	echo "-------------------------$img1,image is saved.-------------------------------"
+	echo "-----------------------------------------------------------------------------"
+	read -p "Enter the name of container you want to run again with saved image:->" con1
+	echo "-----------------------------------------------------------------------------"
+	docker container run -dt --name $con1 $img1
+	echo "-----------------------------------------------------------------------------"
+	echo "----------------------$con1 container is running.----------------------------"
+	echo "-----------------------------------------------------------------------------"
+	docker exec -i $con1 bash -c "dd if=/dev/zero of=/root/file.txt bs=1M count=500"
+	echo "-----------------------------------------------------------------------------"
+	echo "-------------File is created inside container with 500MB size.---------------"
+	echo "-----------------------------------------------------------------------------"
+	read -p "Enter the name of image you want to save container with final chnages:->" img2
+	echo "-----------------------------------------------------------------------------"
+	docker container commit $con1 $img2 
+	echo "-----------------------------------------------------------------------------"
+	echo "---------------------------$img2 , final image is saved----------------------"
+	echo "-----------------------------------------------------------------------------"
+	docker image history $img2 >> $img2.log.txt
+	echo "-----------------------------------------------------------------------------"
+	echo "----------------Image History sent to $img2.log.txt--------------------------"
+	echo "-----------------------------------------------------------------------------"
 	i=`expr $i + 1`
 done
+
